6a28b485a367c17f4fe8b65486567a138bc51ab2
Ushan <ushanmithmakumara@outlook.com>
2023-11-12 11:59:59 +0530

    start creating discord bot

---
+++ src/main.js
@@ -0,0 +1,46 @@
+require('dotenv').config()
+
+const { Client, IntentsBitField, EmbedBuilder } = require('discord.js')
+
+const client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ]
+})
+
+
+client.on("ready", (e) => {
+    console.log(`✅ ${e.user.username} is ready.`)
+})
+
+client.on("messageCreate", (message) => {
+    if (message.author.bot) return
+
+    message.reply('Hello, how can I help you?')
+})
+
+client.on("interactionCreate", (interaction) => {
+    if (!interaction.isChatInputCommand()) return
+
+    if (interaction.commandName == 'download') {
+        interaction.reply("Here your source file")
+    }
+    
+    if (interaction.commandName == 'add') {
+        const num1 = interaction.options.get('first-number').value
+        const num2 = interaction.options.get('second-number').value
+
+        interaction.reply(`The sum is ${num1 + num2}`)
+    }
+    
+    if (interaction.commandName == 'embed') {
+        const embed = new EmbedBuilder().setTitle('Embed title').setDescription('Embed description')
+
+        interaction.reply({ embeds: [embed] })
+    }
+})
+
+client.login(process.env.TOKEN)
---
+++ src/register-commands.js
@@ -0,0 +1,52 @@
+require('dotenv').config()
+
+const { REST, Routes, ApplicationCommandOptionType } = require('discord.js')
+
+const commands = [
+    {
+        name: 'hey',
+        description: 'Replies with hey!'
+    },
+    {
+        name: 'download',
+        description: 'Download source file'
+    },
+    {
+        name: 'add',
+        description: 'Add two numbers',
+        options: [
+            {
+                name: 'first-number', // cannot put whitespace
+                description: 'The first number',
+                type: ApplicationCommandOptionType.Number,
+                choices: [
+                    { name: 'one', value: 1 },
+                    { name: 'two', value: 2 },
+                    { name: 'three', value: 3 },
+                ],
+                required: true,
+            },
+            {
+                name: 'second-number',
+                description: 'The second number',
+                type: ApplicationCommandOptionType.Number,
+                required: true,
+            }
+        ]
+    },
+    {
+        name: 'embed',
+        description: 'Send embed message!'
+    },
+]
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN)
+
+;(async () => {
+    try {
+        await rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), { body: commands })
+        console.log('Slash commands were registed successfully.')
+    } catch (error) {
+        console.log(`There was an error: ${error}`)
+    }
+})()
