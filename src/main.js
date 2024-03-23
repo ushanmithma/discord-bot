1655cb114cc2e2b778caa54904121226367d8c12
Ushan <ushanmithmakumara@outlook.com>
2024-03-23 15:09:07 +0530

    fixing exporting issue

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const { Client, IntentsBitField } = require('discord.js')
 const eventHandler = require('./handlers/eventHandler')
