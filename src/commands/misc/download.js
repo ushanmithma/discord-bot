68bbd7848bcf9eef14fe1c1e27bc317912860f6e
Ushan <ushanmithmakumara@outlook.com>
2024-03-23 14:50:22 +0530

    Add slash commands, Setup vercel

---
+++ src/commands/misc/download.js
@@ -0,0 +1,52 @@
+const { ApplicationCommandOptionType } = require('discord.js')
+const games = require('../../../data/games.json')
+const path = require('path')
+
+module.exports = {
+  name: 'download',
+  description: 'Download game that we play!',
+  options: [
+    {
+      type: ApplicationCommandOptionType.String,
+      name: 'name',
+      description: 'Choose a game that you want to download!',
+      required: true,
+      choices: [
+        { name: 'Palworld', value: 'Palworld' },
+        { name: 'Sea of Thieves', value: 'Sea of Thieves' },
+        { name: 'Valheim', value: 'Valheim' },
+      ],
+    },
+  ],
+  // devOnly: Boolean,
+  // testOnly: true,
+  // options: Object[],
+  // deleted: Boolean,
+
+  callback: (client, interaction) => {
+    let game = games.find(
+      (game) => game.title == interaction.options.get('name').value
+    )
+
+    if (game) {
+      const embed = {
+        title: game.title,
+        color: parseInt(game.color), // Embed color in hexadecimal
+        description: game.description,
+        image: {
+          url: game.image,
+        },
+        timestamp: new Date(),
+      }
+
+      interaction.reply({
+        embeds: [embed],
+        files: [path.join(__dirname, '../../../', game.file)],
+      })
+    } else {
+      interaction.reply(
+        `We don't play ${interaction.options.get('name').value}`
+      )
+    }
+  },
+}
--- src/main.js
+++ src/main.js
@@ -1,50 +1,17 @@
 require('dotenv').config()
 
-const { Client, IntentsBitField, EmbedBuilder } = require('discord.js')
+const { Client, IntentsBitField } = require('discord.js')
+const eventHandler = require('./handlers/eventHandler')
 
 const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-    ]
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
 })
 
-
-client.on("ready", (e) => {
-    console.log(`✅ ${e.user.username} is ready.`)
-})
-
-client.on("messageCreate", (message) => {
-    if (message.author.bot) return
-
-    message.reply('Hello, how can I help you?')
-})
-
-client.on("interactionCreate", (interaction) => {
-    if (!interaction.isChatInputCommand()) return
-
-    if (interaction.commandName == 'download') {
-        interaction.reply("Here your source file")
-    }
-    
-    if (interaction.commandName == 'hey') {
-        interaction.reply(`Hello, ${interaction.user.globalName} ✌️`)
-    }
-    
-    if (interaction.commandName == 'add') {
-        const num1 = interaction.options.get('first-number').value
-        const num2 = interaction.options.get('second-number').value
-
-        interaction.reply(`The sum is ${num1 + num2}`)
-    }
-    
-    if (interaction.commandName == 'embed') {
-        const embed = new EmbedBuilder().setTitle('Embed title').setDescription('Embed description')
-
-        interaction.reply({ embeds: [embed] })
-    }
-})
+eventHandler(client)
 
 client.login(process.env.TOKEN)
